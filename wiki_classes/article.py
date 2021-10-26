24724d007c05b44b1ce0f4fc23f32b8d38b1297c
Ben Schmidt <bmschmidt@gmail.com>
2021-10-25 20:33:43 -0400

    second commit

--- wiki_classes/article.py
+++ wiki_classes/article.py
@@ -1,7 +1,11 @@
 class Article():
+
     def __init__(self, title, text = None):
         self.text = text
         self.title = title
+
     def fill_text(self, wikipedia):
         if self.text is None:
             self.text = wikipedia.get_text(self.title)
+    
+    
--- wiki_classes/wikipedia.py
+++ wiki_classes/wikipedia.py
@@ -1,4 +1,5 @@
 import duckdb
+
 from .article import Article
 
 class Wikipedia():
@@ -18,11 +19,27 @@ class Wikipedia():
         while True:
             title, text = result_set.fetchone()
             yield Article(title, text)
-            
+    
     def __iter__(self):
         for article in self.iter_over_query("SELECT titles, text FROM wiki"):
             yield article
+    
+    def text_article(self, query, limit = 10_000):
+        """
+        Returns an iterator over articles containing the string 'query'.
 
-    def text_article(self, query):
-        for article in self.iter_over_query(f"SELECT titles, text FROM wiki WHERE text LIKE '%query%'"):
+        query: a string to search for across wikipedia.
+        limit: maximum number of articles to return.
+        """
+        for article in self.iter_over_query(f"SELECT titles, text FROM wiki WHERE text LIKE '%{query}%' LIMIT {limit}"):
             yield article
+    
+    def make_collection(self, words = ["environment", "global warming", "climate change", "conservation"]):
+      collection = []
+      seen_articles = {}
+      for word in words:
+        for article in self.text_article(word):
+          if not article.title in seen_articles:
+            collection.append(article)
+            seen_articles[article.title] = True
+      return collection
